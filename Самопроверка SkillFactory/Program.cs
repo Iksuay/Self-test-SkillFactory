194cd4d9e3acd282d5d070cd2def1ea6649b434b
Tamer <tamer.masud@mail.ru>
2024-02-06 21:17:58 +0400

    Добавьте файлы проекта.

---
+++ Самопроверка SkillFactory/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("What your name?");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("What your age?");
+
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+
+        Console.WriteLine("Enter your burthdate");
+        var burthdate = Console.ReadLine();
+        Console.WriteLine("Your berthdate {0}", burthdate);
+
+        Console.Write("What is your favorite day of week? ");
+
+        var day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favorite day is {0}", day);
+
+    }
+}
